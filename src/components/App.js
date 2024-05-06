3c926c6f73460391a470e070096b39e63a190d72
Caren Wanjiru Mwaka Chibwara <carrienjiru40@gmail.com>
2024-05-07 00:30:34 +0300

    modiified App.js, Item.js and ShoppingList.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // State variable to determine whether the app is in dark mode or light mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Function to toggle between dark mode and light mode
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  // Determining the class name based on the current mode
+  const appClass = isDarkMode ? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/* Button to toggle dark mode */}
+        <button onClick={toggleDarkMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable to determine whether the item is in the cart or not
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Function to toggle the item's in-cart state
+  const toggleCartStatus = () => {
+    setIsInCart((prevStatus) => !prevStatus);
+  };
+
+  // Determining the class name based on the cart status
+  const liClassName = isInCart ? "in-cart" : "";
+
+  // Determining the button text based on the cart status
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Button to add/remove item from cart */}
+      <button className="add" onClick={toggleCartStatus}>{buttonText}</button>
     </li>
   );
 }
